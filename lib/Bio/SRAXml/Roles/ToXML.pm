5f7bfd3846acba1ee99565c1fbcd308eff3e0203
davidrichardson <david.m.richardson@gmail.com>
2015-09-25 10:18:46 +0100

    Use params validate on core methods

--- lib/Bio/SRAXml/Roles/ToXML.pm
+++ lib/Bio/SRAXml/Roles/ToXML.pm
@@ -2,14 +2,13 @@ package Bio::SRAXml::Roles::ToXML;
 use strict;
 use Moose::Role;
 use Carp;
+use MooseX::Params::Validate;
+
 requires 'write_to_xml';
 
 before 'write_to_xml' => sub {
-    my ( $self, $xml_writer, ) = @_;
-
-    if ( !$xml_writer || !$xml_writer->isa('XML::Writer') ) {
-        confess("first argument to write_to_xml should be an XML::Writer object");
-    }
+    my ($self) = shift;
+    my ($xml_writer) = pos_validated_list( \@_, { isa => 'XML::Writer' } );
 };
 
 1;
--- lib/Bio/SRAXml/Roles/ToXMLwithTagName.pm
+++ lib/Bio/SRAXml/Roles/ToXMLwithTagName.pm
@@ -2,18 +2,14 @@ package Bio::SRAXml::Roles::ToXMLwithTagName;
 use strict;
 use Moose::Role;
 use Carp;
+use MooseX::Params::Validate;
+
 requires 'write_to_xml';
 
 before 'write_to_xml' => sub {
-    my ( $self, $xml_writer, $tag_name ) = @_;
-
-    if ( !$xml_writer || !$xml_writer->isa('XML::Writer') ) {
-        confess("first argument to write_to_xml should be an XML::Writer object");
-    }
-    
-    if (! defined $tag_name) {
-      confess("second argument to write_to_xml should be a tag name ")
-    }
+    my ($self) = shift;
+    my ( $xml_writer, $tag_name ) =
+      pos_validated_list( \@_, { isa => 'XML::Writer' }, { isa => 'Str' } );
 };
 
 1;
